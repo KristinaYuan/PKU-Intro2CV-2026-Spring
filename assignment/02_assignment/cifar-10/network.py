e7c2c8323278ea937e602649ffc5d1cc0cefa042
KristinaYuan <205441573+KristinaYuan@users.noreply.github.com>
2026-04-25 16:34:39 +0800

    Assignment 02

---
+++ assignment/02_assignment/cifar-10/network.py
@@ -0,0 +1,67 @@
+import torch.nn as nn
+
+
+class ConvNet(nn.Module):
+    def __init__(self, num_class=10):
+        super(ConvNet, self).__init__()
+        
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, padding=1)
+        self.bn1 = nn.BatchNorm2d(32)
+        self.relu1 = nn.ReLU(inplace=True)
+        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
+        
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
+        self.bn2 = nn.BatchNorm2d(64)
+        self.relu2 = nn.ReLU(inplace=True)
+        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
+        
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
+        self.bn3 = nn.BatchNorm2d(128)
+        self.relu3 = nn.ReLU(inplace=True)
+        self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2)
+        
+        self.flatten = nn.Flatten()
+        self.fc1 = nn.Linear(128 * 4 * 4, 256)
+        self.relu4 = nn.ReLU(inplace=True)
+        self.dropout = nn.Dropout(0.5)
+        self.fc2 = nn.Linear(256, num_class)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu1(x)
+        x = self.pool1(x)
+        
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x = self.relu2(x)
+        x = self.pool2(x)
+        
+        x = self.conv3(x)
+        x = self.bn3(x)
+        x = self.relu3(x)
+        x = self.pool3(x)
+        
+        x = self.flatten(x)
+        x = self.fc1(x)
+        x = self.relu4(x)
+        x = self.dropout(x)
+        x = self.fc2(x)
+        return x
+
+
+if __name__ == '__main__':
+    import torch
+    from torch.utils.tensorboard  import SummaryWriter
+    from dataset import CIFAR10
+    writer = SummaryWriter(log_dir='../experiments/network_structure')
+    net = ConvNet()
+    train_dataset = CIFAR10()
+    train_loader = torch.utils.data.DataLoader(
+        train_dataset, batch_size=2, shuffle=False, num_workers=2)
+    # Write a CNN graph. 
+    # Please save a figure/screenshot to '../results' for submission.
+    for imgs, labels in train_loader:
+        writer.add_graph(net, imgs)
+        writer.close()
+        break 
